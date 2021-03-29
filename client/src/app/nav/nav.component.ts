838acc04647181aab221d643e94de809787fa0de
Niran Vijayakumar <niranvv@gmail.com>
2021-03-29 22:55:25 +0530

    68 Added Routes & Toastr

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -9,24 +11,29 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  model : any = {}
+  model: any = {}
 
-  constructor(public accountService : AccountService) { }
+  constructor(public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
 
-  login(){
+  login() {
     console.log(this.model);
     this.accountService.login(this.model).subscribe(respose => {
       console.log(respose);
-    },error => {
+      this.router.navigateByUrl('/members');
+    }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     });
   }
 
-  logout(){
+  logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
