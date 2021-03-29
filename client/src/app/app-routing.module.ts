e350e8a9f150103f85fcab9d89531eca13901002
Niran Vijayakumar <niranvv@gmail.com>
2021-03-29 23:40:01 +0530

    Session 6 COmpleted Shared Modue

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
@@ -15,7 +16,7 @@ const routes: Routes = [
     children: [{ path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
     { path: 'members/:id', component: MemberDetailComponent },
     { path: 'lists', component: ListsComponent },
-    { path: 'messages', component: MemberListComponent }]
+    { path: 'messages', component: MessagesComponent }]
   },
   { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
