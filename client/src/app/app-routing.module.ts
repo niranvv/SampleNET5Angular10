82fad6cf2b085cc7d4e27ffb6a53feb8be0ca420
Niran Vijayakumar <niranvv@gmail.com>
2021-03-29 23:08:31 +0530

    68 Add Route Guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'members', component: MemberListComponent},
+  {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
   {path: 'members/:id', component: MemberDetailComponent},
   {path: 'lists', component: ListsComponent},
   {path: 'messages', component: MemberListComponent},
