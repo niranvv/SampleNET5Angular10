9ab6883fe75a50c8da033ea077bef7fcb89af111
Niran Vijayakumar <niranvv@gmail.com>
2021-04-13 21:45:48 +0530

    Section 8 - 97 Automapper Queryable extensions

--- API/Helpers/AutomapperProfiles.cs
+++ API/Helpers/AutomapperProfiles.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using AutoMapper;
 
 namespace API.Helpers
@@ -13,7 +14,11 @@ namespace API.Helpers
                 .ForMember(
                     dest => dest.PhotoUrl,
                     opt => opt.MapFrom(src => src.Photos
-                                                    .FirstOrDefault(x => x.IsMain).Url));
+                                                    .FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(
+                    dest => dest.Age,
+                    opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge())
+                );
             CreateMap<Photo, PhotoDto>();
         }
     }
