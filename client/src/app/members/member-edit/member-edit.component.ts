c85e976031b0558efc67293d3e8ea0c5e4bd0ba4
Niran Vijayakumar <niranvv@gmail.com>
2021-04-18 19:19:25 +0530

    119 Updating user in Angular API Integration

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -40,8 +40,11 @@ export class MemberEditComponent implements OnInit {
 
   updateMember() {
     console.log(this.member);
-    this.toastr.success('Profile Updated Successfully');
-    this.editForm.reset(this.member);
+    this.memberService.updateMember(this.member).subscribe(() => {
+      this.toastr.success('Profile Updated Successfully');
+      this.editForm.reset(this.member);
+    })
+    
   }
 
 }
