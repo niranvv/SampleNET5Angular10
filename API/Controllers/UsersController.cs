57170fcbf4557f58e3a93e1ef251b2386a1e1b31
Niran Vijayakumar <niranvv@gmail.com>
2021-04-21 21:22:06 +0530

    129 using CreatedAt in response

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -35,7 +35,7 @@ namespace API.Controllers
             return Ok(users);
         }
 
-        [HttpGet("{username}")]
+        [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await _userRepository.GetMemberAsync(username);
@@ -57,18 +57,21 @@ namespace API.Controllers
         {
             var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
             var result = await _photoService.AddPhotoAsync(file);
-            if(result.Error != null) return BadRequest(result.Error.Message);
-            var photo=new Photo{
-                Url=result.SecureUrl.AbsoluteUri,
-                PublicId=result.PublicId
+            if (result.Error != null) return BadRequest(result.Error.Message);
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
             };
 
-            if(user.Photos.Count==0) photo.IsMain=true;
+            if (user.Photos.Count == 0) photo.IsMain = true;
 
             user.Photos.Add(photo);
 
-            if(await _userRepository.SaveAllAsync())
-                return _mapper.Map<PhotoDto>(photo);
+            if (await _userRepository.SaveAllAsync())
+            {
+                return CreatedAtRoute("GetUser", new { username = user.UserName }, _mapper.Map<PhotoDto>(photo));
+            }
 
             return BadRequest("Problem adding photo");
         }
