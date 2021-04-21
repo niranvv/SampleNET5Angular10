f8ff4a85ef9049ce83db333dd79d5449de3a6b14
Niran Vijayakumar <niranvv@gmail.com>
2021-04-22 00:26:18 +0530

    section 11(138) Delete the photos from Angular

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -41,6 +41,12 @@ export class PhotoEditorComponent implements OnInit {
     })
   }
 
+  deletePhoto(photoId:number){
+    this.memberService.deletePhoto(photoId).subscribe(() => {
+      this.member.photos=this.member.photos.filter(x => x.id !== photoId);
+    });
+  }
+
   fileOverBase(e: any) {
     this.hasBaseDropzoneOver = e;
   }
