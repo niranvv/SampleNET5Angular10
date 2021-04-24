b3a98668315508663402179900023e8f79d1c28c
Niran Vijayakumar <niranvv@gmail.com>
2021-04-24 23:40:35 +0530

    Section 12 (149) Client Side Registration integration

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -9,19 +10,19 @@ import { AccountService } from '../_services/account.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  // @Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
   registerForm: FormGroup;
-  maxDate:Date;
+  maxDate: Date;
+  validationErrors: string[]=[];
 
   constructor(private accountService: AccountService,
     private toastr: ToastrService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.initializeForm();
-    this.maxDate=new Date();
+    this.maxDate = new Date();
     this.maxDate.setFullYear(this.maxDate.getFullYear() - 18)
   }
 
@@ -50,13 +51,12 @@ export class RegisterComponent implements OnInit {
 
   register() {
     console.log(this.registerForm.value);
-    // this.accountService.register(this.model).subscribe(response => {
-    //   console.log(response);
-    //   this.cancel();
-    // }, error => {
-    //   console.log(error);
-    //   this.toastr.error(error.error);
-    // });
+    this.accountService.register(this.registerForm.value).subscribe(response => {
+      this.router.navigateByUrl('/members');
+    }, error => {
+      console.log(error);
+      this.validationErrors = error;
+    });
   }
 
   cancel() {
