6b6f9a9f83ca0824c489586b2829a2e42c5afe2c
Niran Vijayakumar <niranvv@gmail.com>
2021-05-03 23:50:06 +0530

    Section 13 - 168 Remembering filters in the Service

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -18,14 +18,10 @@ export class MemberListComponent implements OnInit {
   pagination: Pagination;
   userParams: UserParams;
   user: User;
-  genderList=[{value: 'male', display: 'Males'},{value: 'female',display:'Females'}];
+  genderList = [{ value: 'male', display: 'Males' }, { value: 'female', display: 'Females' }];
 
-  constructor(private memberService: MembersService, private accountService: AccountService) {
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user => {
-      this.user = user,
-        this.userParams = new UserParams(user)
-    })
-
+  constructor(private memberService: MembersService) {
+    this.userParams = this.memberService.getUserParams();
   }
 
   ngOnInit(): void {
@@ -33,19 +29,21 @@ export class MemberListComponent implements OnInit {
   }
 
   loadMembers() {
+    this.memberService.setUserParams(this.userParams);
     this.memberService.getMembers(this.userParams).subscribe(response => {
       this.members = response.result;
       this.pagination = response.pagination;
     });
   }
 
-  resetFilter(){
-    this.userParams=new UserParams(this.user);
+  resetFilter() {
+    this.userParams = this.memberService.resetUserparams();
     this.loadMembers();
   }
 
   pageChanged(event: any) {
     this.userParams.pageNumber = event.page;
+    this.memberService.setUserParams(this.userParams);
     this.loadMembers();
   }
 }
