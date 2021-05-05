adfeb6f74f9474e7487fad814dc1611369a78a2b
Niran Vijayakumar <niranvv@gmail.com>
2021-05-05 20:04:47 +0530

    186 Getting message thread for 2 users

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -60,5 +60,12 @@ namespace API.Controllers
             Response.AddPaginationHeader(messages.CurrentPage, messages.PageSize, messages.TotalCount, messages.TotalPages);
             return messages;
         }
+
+        [HttpGet("thread/{username}")]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
+        {
+            var currentUsername = User.GetUsername();
+            return Ok(await _messageRepository.GetMessageThread(currentUsername, username));
+        }
     }
 }
