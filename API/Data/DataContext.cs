2d4ae9fc21b42114992b82b6650973c40d9fcf37
Niran Vijayakumar <niranvv@gmail.com>
2021-05-06 23:34:07 +0530

    201 Identity - Configuring DbContext

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,21 +1,42 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser,
+                                                    AppRole,
+                                                    int,
+                                                    IdentityUserClaim<int>,
+                                                    AppUserRole,
+                                                    IdentityUserLogin<int>,
+                                                    IdentityRoleClaim<int>,
+                                                    IdentityUserToken<int>>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
         }
 
-        public DbSet<AppUser> Users { get; set; }
+        //public DbSet<AppUser> Users { get; set; }
         public DbSet<UserLike> Likes { get; set; }
         public DbSet<Message> Messages { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<AppUser>()
+                        .HasMany(ur => ur.UserRoles)
+                        .WithOne(u => u.User)
+                        .HasForeignKey(ur => ur.UserId)
+                        .IsRequired();
+
+            builder.Entity<AppRole>()
+                        .HasMany(ur => ur.UserRoles)
+                        .WithOne(u => u.Role)
+                        .HasForeignKey(ur => ur.RoleId)
+                        .IsRequired();
 
             builder.Entity<UserLike>()
                         .HasKey(k => new { k.SourceUserId, k.LikedUserId });
@@ -25,18 +46,18 @@ namespace API.Data
                         .WithMany(l => l.LikedUsers)
                         .HasForeignKey(s => s.SourceUserId)
                         .OnDelete(DeleteBehavior.Cascade);
-             
+
             builder.Entity<UserLike>()
                         .HasOne(s => s.LikedUser)
                         .WithMany(l => l.LikedByUsers)
                         .HasForeignKey(s => s.LikedUserId)
                         .OnDelete(DeleteBehavior.Cascade);
-            
+
             builder.Entity<Message>()
                         .HasOne(u => u.Recipient)
                         .WithMany(m => m.MessagesReceived)
                         .OnDelete(DeleteBehavior.Restrict);
-            
+
             builder.Entity<Message>()
                         .HasOne(u => u.Sender)
                         .WithMany(m => m.MessagesSent)
