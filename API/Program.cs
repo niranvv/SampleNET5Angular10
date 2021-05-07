f1b3cc4f669e353dd4f7a8728b533fcd8ec5e3eb
Niran Vijayakumar <niranvv@gmail.com>
2021-05-07 15:51:58 +0530

    206 Identity - Adding Roles to the Seed Data

--- API/Program.cs
+++ API/Program.cs
@@ -18,20 +18,21 @@ namespace API
     {
         public static async Task Main(string[] args)
         {
-            var host=CreateHostBuilder(args).Build();
-            using var scope=host.Services.CreateScope();
-            var services=scope.ServiceProvider;
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
             try
             {
-                var context=services.GetRequiredService<DataContext>();
+                var context = services.GetRequiredService<DataContext>();
                 var userManager = services.GetRequiredService<UserManager<AppUser>>();
+                var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
                 await context.Database.MigrateAsync();
-                await Seed.SeedUsers(userManager);
+                await Seed.SeedUsers(userManager, roleManager);
             }
             catch (Exception ex)
             {
-                var logger=services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex,"An error occured during migration");
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occured during migration");
             }
             await host.RunAsync();
         }
