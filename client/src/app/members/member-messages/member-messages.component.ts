fbb06d633c2c90564a93bbba30354a74634317d7
Niran Vijayakumar <niranvv@gmail.com>
2021-05-10 12:47:16 +0530

    End of Course - Adding loadingindicator on Hub calls using busyService

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -14,6 +14,7 @@ export class MemberMessagesComponent implements OnInit {
   @Input() messages: Message[];
   @Input() username: string;
   messageContent: string;
+  loading = false;
 
   constructor(public messageService: MessageService) { }
 
@@ -21,10 +22,11 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   sendMessage() {
+    this.loading = true;
     this.messageService.sendMessage(this.username, this.messageContent).then(() => {
       //this.messages.push(message);
       this.messageForm.reset();
-    });
+    }).finally(() => this.loading = false);
   }
 
 }
